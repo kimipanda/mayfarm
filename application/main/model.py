1311b77bdf5d80f7c4414b7b261ddddbb6814542
Euihyun Kim <nodamegood@gmail.com>
2019-10-28 03:15:22 +0900

    create model

--- application/main/model.py
+++ application/main/model.py
@@ -1,7 +1,17 @@
-from application import app, db
-from datetime import datetime
+from application import db
 
 
-class Mayfarm(db.Model):
-    __tablename__ = 'token'
-    id = db.Colmun(db.Integer, primary_key=True)
+class Problems(db.Model):
+    __tablename__ = 'problem_table'
+    id = db.Column(db.Integer, primary_key=True)
+    problem_type1 = db.Column(db.String)
+    problem_type2 = db.Column(db.String)
+    problem_sentence = db.Column(db.String)
+    teacher = db.Column(db.String)
+    problems_model_columns = [
+        'id',
+        'problem_type1',
+        'problem_type2',
+        'problem_sentence',
+        'teacher'
+    ]
